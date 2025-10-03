1baf3faa43791812a134200e84a92052b5c04a08
Nikita Lebedev <l3bedev.n@yandex.ru>
2025-10-04 00:52:59 +0300

    Подключение Tailwind и настройка стилей

---
+++ src/components/shared/container.tsx
@@ -0,0 +1,10 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+interface Props {
+  className?: string;
+}
+
+export const Container: React.FC<React.PropsWithChildren<Props>> = ({ className, children }) => {
+  return <div className={cn("max-w-[1280px] mx-auto px-4 sm:px-6 lg:px-8", className)}>{children}</div>;
+};
